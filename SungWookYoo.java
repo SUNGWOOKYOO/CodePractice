b75d654bcdb8cae57d88d6cb2d83ce6174c62a85
SUNGWOOKYOO <34184898+SUNGWOOKYOO@users.noreply.github.com>
2018-09-18 20:56:16 +0900

    add java pratice

---
+++ SungWookYoo.java
@@ -0,0 +1,11 @@
+package jump2java.house.person;
+
+import jump2java.house.HousePark;	
+
+public class SungWookYoo extends HousePark {
+	public static void main(String[] args){
+		SungWookYoo swy = new SungWookYoo();
+		System.out.println(swy.lastname);
+	}
+
+}
